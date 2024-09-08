b9f6ec872a960b4843f482650d4910bf844bf6ad
ArunKushhhh <arunkushwaha_230604@gmail.edu.in>
2024-09-09 03:03:55 +0530

    Sidebar images added

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,8 +1,16 @@
+// import React from "react";
+import RegisterNowBG from "../../assets/RegisterNowBG.png"; 
+import RulebookDownload from "../../assets/Unnati_image.png"; 
+
 const SideBar = () => {
   return (
     <div className="w-[322px] h-[811px] mt-[16px]">
       <div>
-        <div className="h-[315px] pl-[59.71px] pr-[59.72px] pt-[237px] pb-[26px] bg-black rounded-2xl justify-center items-center inline-flex">
+        <div className="h-[315px] pl-[59.71px] pr-[59.72px] pt-[237px] pb-[26px] bg-black rounded-2xl justify-center items-center inline-flex" style={{
+            backgroundImage: `url(${RegisterNowBG})`, // Set background image
+            backgroundSize: "cover", // Ensure it covers the entire div
+            backgroundPosition: "center", // Center the background
+          }}>
           <div className="grow shrink basis-0 h-[52px] bg-white rounded-[40px] justify-center items-center inline-flex">
             <div className="pl-4 pr-2 py-4 rounded-[40px] justify-center items-center gap-2 inline-flex">
               <div className="justify-center items-center gap-2.5 flex">
@@ -16,12 +24,16 @@ const SideBar = () => {
         </div>
       </div>
       <div>
-      <div className="h-[472px] pl-[59.71px] pr-[59.72px] pt-[237px] pb-[26px] bg-black rounded-2xl justify-center items-center inline-flex mt-[16px]">
+      <div className="h-[472px] pl-[59.71px] pr-[59.72px] pt-[237px] pb-[26px] bg-black rounded-2xl justify-center items-center inline-flex mt-[16px]" style={{
+            backgroundImage: `url(${RulebookDownload})`, // Set background image
+            backgroundSize: "cover", // Ensure it covers the entire div
+            backgroundPosition: "center", // Center the background
+          }}>
           <div className="grow shrink basis-0 h-[52px] bg-white rounded-[40px] justify-center items-center inline-flex">
             <div className="pl-4 pr-2 py-4 rounded-[40px] justify-center items-center gap-2 inline-flex">
               <div className="justify-center items-center gap-2.5 flex">
                 <div className="text-black text-lg font-normal f    ont-['Inter']">
-                  Register Now
+                  Download Rulebook
                 </div>
               </div>
               <div className="w-10 h-10 origin-top-left -rotate-45 rounded-[50px] flex-col justify-center items-center gap-2.5 inline-flex"></div>
