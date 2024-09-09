be8a76404fee2e7918cf3d6657875a01c18775b3
swarupnihinmedia <swarup.baidya@nihinmedia.jp>
2024-09-09 14:31:06 +0530

    done commiting

--- src/components/rulebook/ruleBook.jsx
+++ src/components/rulebook/ruleBook.jsx
@@ -3,7 +3,6 @@ import CardTwo from './card2';
 
 const RuleBook = () => {
   return (
-<<<<<<< HEAD
     <div className="w-full max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
       <div className="flex flex-col sm:flex-col lg:flex-row gap-6 sm:gap-[10%]">
         <div className="w-full sm:w-1/2 flex sm:justify-center mb-0 sm:mb-4">
@@ -13,11 +12,6 @@ const RuleBook = () => {
           <CardTwo />
         </div>
       </div>
-=======
-    <div className='w-[100%] h-[650px] flex gap-[80px] justify-center items-center'>
-        <CardOne/>
-        <CardTwo/>
->>>>>>> ed02fbdafca8e330570f77b74ec55a3f11f9d06d
     </div>
   );
 };
