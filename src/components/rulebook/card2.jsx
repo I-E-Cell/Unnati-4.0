486389b6522e077cf086f25df1d9facf95f9c917
Mitesh2806 <miteshmaity_22519@aitpune.edu.in>
2024-09-10 15:38:49 +0530

    Responsiveness

--- src/components/rulebook/card2.jsx
+++ src/components/rulebook/card2.jsx
@@ -2,7 +2,7 @@ import { CardRuleBook } from "../../ui/card-rulebook";
 
 const card2 = () => {
   return (
-    <div className="relative bg-transparent w-[440px] h-[550px] rounded-xl">
+    <div className="relative bg-transparent w-[100%] md:w-[440px] h-[660px] md:h-[550px] rounded-xl">
       <div
         className="rulebookGradient w-[120px] xs:h-[300px] lg:h-[605px] rotate-[-105.74deg] rounded-[645px] opacity-[0.5] blur-[95px] absolute right-32 top-48"
         style={{
